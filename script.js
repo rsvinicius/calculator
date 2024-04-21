583f78d4c472a9f0070092d1073ccf732733bc57
Vinicius R <vrodrigues.code@gmail.com>
2024-04-21 10:48:02 -0300

    chore: add operate function that takes an operator and 2 numbers and then calls one of the basic math functions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(n1, n2, operator) {
+    switch(operator) {
+        case '+':
+            return add(n1,n2);
+        case '-':
+            return subtract(n1,n2);
+        case '*':
+            return multiply(n1,n2);
+        case '/':
+            return divide(n1,n2);
+        default:
+            return "OOPS... Invalid operator!"
+    }
+}
+
 const add = function (a, b) {
     return a + b;
 };
